9fc7a1eaf6aac6fa94d20a902070dbf34f84d34b
Nightrose2016 <lrbarrer@gmail.com>
2020-12-22 00:41:54 -0500

    generated timeblock resource

---
+++ app/controllers/timeblocks_controller.rb
@@ -0,0 +1,2 @@
+class TimeblocksController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :timeblocks
   resources :years
   resources :months
   resources :weeks

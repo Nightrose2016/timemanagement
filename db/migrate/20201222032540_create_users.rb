5b80ed15597bece74c7972bb520663a4ec67964f
Nightrose2016 <lrbarrer@gmail.com>
2020-12-22 01:58:04 -0500

    added email to user migration

--- db/migrate/20201222032540_create_users.rb
+++ db/migrate/20201222032540_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :username , null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :email, null: false, default: ""
+      
       t.timestamps
     end
   end
